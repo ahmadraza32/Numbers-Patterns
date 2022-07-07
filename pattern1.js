540160ed1ae444ed954fca1ad1b49a6706ac777e
ahmadraza32 <ahmedraza32125@gmail.com>
2022-07-07 01:48:16 -0700

    initial commit

---
+++ pattern1.js
@@ -0,0 +1,17 @@
+
+
+for(let i=1; i<=5; i++){ // for printing rows
+
+    for(let j=1; j<=i; j++){ // for columns
+        document.write(j, " ") 
+    }
+    document.write("<br />")
+}
+
+// this program will print this pattern
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
---
+++ pattern3.js
@@ -0,0 +1,17 @@
+
+let counter = 0
+for(let i=1; i<= 5; i++){
+    for(let j=1; j<=i; j++){
+        document.write(counter, " ")
+        counter++
+    }
+    document.write("<br />")
+}
+
+// printing result
+
+// 0
+// 1 2
+// 3 4 5
+// 6 7 8 9
+// 10 11 12 13 14
